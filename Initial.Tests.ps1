29f8584f35bb9cf74b968ccd4e623178ebf33474
Bartek B <bartek.bielawski@live.com>
2018-02-16 10:31:52 +0000

    Removing (intentionally) broken test.

--- Initial.Tests.ps1
+++ Initial.Tests.ps1
@@ -2,8 +2,4 @@ Describe 'Testing YinYang presentation' {
     It 'Given BrokenScript is called, it throws exception' {
         { .\BrokenScript.ps1 } | Should Throw
     }
-
-    It 'Given I have broken test, build fails' {
-        0 | Should Be 1
-    }
 }
